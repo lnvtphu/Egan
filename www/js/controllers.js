a6b1d57d43b1f7a4c75bd6ace00306cb5a77d2d9
INI\phu.le <phu.le@terralogic.com>
2016-09-16 17:34:31 +0700

    test get data from server

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Chats, $http) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -10,8 +10,15 @@ angular.module('starter.controllers', [])
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
+  $http.get("https://mimomi.herokuapp.com/movie")
+   .then(function(response) {
+      //  $scope.myWelcome = response.data;
+      console.log(response.data);
+      $scope.check = response.data[0].title;
+   });
 
   $scope.chats = Chats.all();
+  console.log(Chats.all());
   $scope.remove = function(chat) {
     Chats.remove(chat);
   };
